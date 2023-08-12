da4bcfafc863fee79b0497b5ea0277570827d52f
taeju <taejugim297@gmail.com>
2023-08-12 12:33:44 +0900

    ..

--- theulim/src/App.tsx
+++ theulim/src/App.tsx
@@ -1,10 +1,6 @@
-import './App.css';
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      김태주 피파 개못
-    </div>
-  );
+  return <div className="App">김태주 피파 개못</div>;
 }
 
 export default App;
