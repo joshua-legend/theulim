6f6f770a02a76b6cf90af33c60d3170692dc504a
박한 <bagkan380@gmail.com>
2023-08-13 11:24:21 +0900

    라우터 추가됨

--- theulim/src/App.tsx
+++ theulim/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
+
 import Kimtaeju from './kimtaeju/kimtaeju';
+import FontList from './parkhan/fontList/font';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  return;
-  <div>
-    <Kimtaeju></Kimtaeju>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <FontList></FontList>
+        <Kimtaeju></Kimtaeju> */}
+        <Routes>
+          <Route path="/kimtaeju" element={<Kimtaeju />}></Route>
+          <Route path="/parkhan" element={<FontList />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
