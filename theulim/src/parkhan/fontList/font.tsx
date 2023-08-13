7512c769f9fd40cf028b0f2d97dbce77b7e40252
박한 <bagkan380@gmail.com>
2023-08-13 11:49:22 +0900

    ..

---
+++ theulim/src/parkhan/fontList/font.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './font.css';
+const FontList = () => {
+  return (
+    <>
+      <div className="NanumBarunpen">
+        <p className="NanumBarunpenL">안녕하세요 font: NanumBarunpen</p>
+        <p className="NanumBarunpenM">안녕하세요 font: NanumBarunpen</p>
+        <p className="NanumBarunpenS">안녕하세요 font: NanumBarunpen</p>
+      </div>
+
+      <div className="KBIZHanmaumMyungjo">
+        <p className="KBIZHanmaumMyungjoL">
+          안녕하세요 font: KBIZHanmaumMyungjo
+        </p>
+        <p className="KBIZHanmaumMyungjoM">
+          안녕하세요 font: KBIZHanmaumMyungjo
+        </p>
+        <p className="KBIZHanmaumMyungjoS">
+          안녕하세요 font: KBIZHanmaumMyungjo
+        </p>
+      </div>
+
+      <div className="KyoboHandwriting2021sjy">
+        <p className="KyoboHandwriting2021sjyL">
+          안녕하세요 font: KyoboHandwriting2021sjy
+        </p>
+        <p className="KyoboHandwriting2021sjyM">
+          안녕하세요 font: KyoboHandwriting2021sjy
+        </p>
+        <p className="KyoboHandwriting2021sjyS">
+          안녕하세요 font: KyoboHandwriting2021sjy
+        </p>
+      </div>
+
+      <div className="jejugothic">
+        <p className="jejugothicL">안녕하세요 font: jejugothic</p>
+        <p className="jejugothicM">안녕하세요 font: jejugothic</p>
+        <p className="jejugothicS">안녕하세요 font: jejugothic</p>
+      </div>
+
+      <div className="GangwonState">
+        <p className="GangwonStateL">안녕하세요 font: GangwonState</p>
+        <p className="GangwonStateM">안녕하세요 font: GangwonState</p>
+        <p className="GangwonStateS">안녕하세요 font: GangwonState</p>
+      </div>
+
+      <div className="NotoSerifKR">
+        <p className="NotoSerifKRL">안녕하세요 font: NotoSerifKRS</p>
+        <p className="NotoSerifKRM">안녕하세요 font: NotoSerifKRS</p>
+        <p className="NotoSerifKRS">안녕하세요 font: NotoSerifKRS</p>
+      </div>
+    </>
+  );
+};
+
+export default FontList;
