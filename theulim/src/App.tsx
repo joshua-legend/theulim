8ad33101d576d7c3072afb4d35ad3cecff43847c
taeju <taejugim297@gmail.com>
2023-08-13 11:52:12 +0900

    router changed

--- theulim/src/App.tsx
+++ theulim/src/App.tsx
@@ -2,15 +2,15 @@ import './App.css';
 import ColorBox from './components/storyboard/kimtaeju/ColorBox';
 import Title from './components/storyboard/kimtaeju/title';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import FontList from './parkhan/fontList/font';
+import ToneAndManner from './components';
 function App() {
   return (
     <div>
       <BrowserRouter>
-        {/* <FontList></FontList>
-        <Kimtaeju></Kimtaeju> */}
         <Routes>
-          {/* <Route path="/" element={<Kimtaeju />}></Route> */}
-          <Route path="/parkhan" element={<FontList />}></Route>
+          <Route path="/tonandmanner" element={<ToneAndManner />}></Route>
+          <Route path="/fontlist" element={<FontList />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- theulim/src/components/index.tsx
+++ theulim/src/components/index.tsx
@@ -1,1 +1,32 @@
-export {}
+import ColorBox from './storyboard/kimtaeju/ColorBox';
+import Title from './storyboard/kimtaeju/title';
+
+const ToneAndManner = () => {
+  const arrangement: React.CSSProperties = {
+    display: `flex`,
+    justifyContent: 'space-evenly',
+    width: '100%',
+    marginBottom: '5%',
+  };
+  return (
+    <>
+      <Title></Title>
+      <div style={arrangement}>
+        <ColorBox Hex="#2c7d52"></ColorBox>
+        <ColorBox Hex="#d35400"></ColorBox>
+        <ColorBox Hex="#3498db"></ColorBox>
+        <ColorBox Hex="#e74c3c"></ColorBox>
+        <ColorBox Hex="#8e44ad"></ColorBox>
+      </div>
+      <div style={arrangement}>
+        <ColorBox Hex="#27ae60"></ColorBox>
+        <ColorBox Hex="#f7dc6f"></ColorBox>
+        <ColorBox Hex="#1abc9c"></ColorBox>
+        <ColorBox Hex="#e67e22"></ColorBox>
+        <ColorBox Hex="#d35400"></ColorBox>
+      </div>
+    </>
+  );
+};
+
+export default ToneAndManner;
