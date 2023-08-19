bcfee6477d4087bc65be0522509c4f4f44127aa2
박한 <bagkan380@gmail.com>
2023-08-19 10:20:17 +0900

    img

--- theulim/src/App.tsx
+++ theulim/src/App.tsx
@@ -3,12 +3,15 @@ import ColorBox from './components/storyboard/kimtaeju/ColorBox';
 import Title from './components/storyboard/kimtaeju/title';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FontList from './parkhan/fontList/font';
+
 import ToneAndManner from './components';
+import MobileFirst from './parkhan/mobileFirst/mobileFirst';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/mobileFirst" element={<MobileFirst />}></Route>
           <Route path="/tonandmanner" element={<ToneAndManner />}></Route>
           <Route path="/fontlist" element={<FontList />}></Route>
         </Routes>
---
+++ theulim/src/parkhan/mobileFirst/mobileFirst.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import './mobileFirst.css';
+const MobileFirst = () => {
+  return (
+    <>
+      <div className="firstSection">
+        <div className="introduce">
+          <p>우리는 더울림 입니다.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MobileFirst;
