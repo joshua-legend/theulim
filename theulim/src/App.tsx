c4d1c1c032da00cec0fa6e5fa66a5e6c426cd200
taeju <taejugim297@gmail.com>
2023-08-20 10:29:20 +0900

    kl

--- theulim/src/App.tsx
+++ theulim/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FontList from './parkhan/fontList/font';
-import ToneAndManner from './components';
-import Pcboard from './components/pcboard/pcboard';
+import ToneAndManner from './design/color';
 import MobileFirst from './parkhan/mobileFirst/mobileFirst';
+import Pcboard from './components/pcboard/pcboard';
 function App() {
   return (
     <div>
